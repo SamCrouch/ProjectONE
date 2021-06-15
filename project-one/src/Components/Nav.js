8715b0c7ce65861a4fd1c0446ede98cdf1c2513d
BlackPowerade <esam3463@gmail.com>
2021-06-15 16:06:42 -0600

    Init nav bar
    Co-authored-by: SamCrouch <SamCrouch@users.noreply.github.com>
    Co-authored-by: kellraym <kellraym@users.noreply.github.com>

---
+++ project-one/src/Components/Nav.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Nav() {
+
+  
+
+  return(
+      <nav>
+        <Link to="/blackjack">Blackjack</Link>
+      </nav>
+
+  )
+}
+
+export default Nav;
