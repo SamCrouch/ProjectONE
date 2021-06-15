343e074f98d34a22e116e72012a9625b5fe0a86c
BlackPowerade <esam3463@gmail.com>
2021-06-15 16:11:23 -0600

    init tests

--- project-one/src/App.test.js
+++ project-one/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Blackjack option', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Blackjack/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test(`URL contains /blackjack when Blackjack is selected`, () => {
+//   render(<App />);
+//   userEvent.click(screen.getByText('Blackjack'))
+//   expect(window.location.pathname).toHaveTextContent('/blackjack')
+// })
+
+
