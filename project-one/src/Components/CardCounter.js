300896886f996521d339537703632437274e536c
BlackPowerade <esam3463@gmail.com>
2021-06-16 14:49:43 -0600

    New counting logic

--- project-one/src/Components/CardCounter.js
+++ project-one/src/Components/CardCounter.js
@@ -3,23 +3,13 @@ import DeckContext from '../deckContext'
 
 function CardCounter(props) {
   const {cardValues} = useContext(DeckContext)
-  
-    const count = props.hand.map((card) => cardValues[card[0]])
-    // does not work with chained reducer
-    function reduce(arr=[1, 1]) {
-      let result = 0
-  
-      for (let i = 0; i < arr.length; i++) {
-        result += arr[i]
-      }
-  
-      return result
-    }
-    
-    const output = reduce(count);
+  const count = props.hand.map((card) => cardValues[card[0]])
 
   return(
-    <div>{output < 22 ? output : `you lose`}</div>
+    // <div>{output < 22 ? output : `you lose`}</div>
+    <div>
+      {props.hand.map(card => <img src={`https://deckofcardsapi.com/static/img/${card}.png`} />)}
+    </div>
   )
 
 }
